0f79268ae3147ed8df40173a2dfad21fd3d8bc1a
Pumamick1 <m.short1@rgu.ac.uk>
2023-10-26 18:12:33 +0100

    Can now dynamically handle Dialogflow requests

--- webhook.py
+++ webhook.py
@@ -5,6 +5,17 @@ import re
 app = Flask(__name__)
 latest_message = ""
 
+def extract_entities_from_payload(payload):
+    # Extract the parameters (entities) from the payload
+    parameters = payload.get("queryResult", {}).get("parameters", {})
+    
+    # Find the entities that have values and store their names as strings, excluding 'geo-country'
+    entity_names = [name for name, value in parameters.items() if value and name != 'geo-country']
+
+    return entity_names
+
+
+
 @app.route('/webhook', methods=['POST', 'GET'])
 def webhook():
     global latest_message
@@ -15,8 +26,13 @@ def webhook():
         api_base_url = 'https://www.gov.uk/api/content/foreign-travel-advice/{}/entry-requirements'
         geo_country = data['queryResult']['parameters']['geo-country'] # Country the user is enquiring about
         api_specific_url = api_base_url.format(geo_country.lower())
+    
         
-        print(data) 
+        #Fetch entity names from payload 
+        entity_names = extract_entities_from_payload(data)
+        entity_keyword = entity_names[0]
+        print(entity_names[0])
+
 
         # Fetching data from the UK foreign office API
         response_API = requests.get(api_specific_url)
@@ -28,18 +44,22 @@ def webhook():
                 html_content = item.get('body', '')
                 break
 
-    #!!!!!!! MATCH DIALOGFLOW ENTITY WITH H-TAG IDs IN A DICT, THEN APPEND TO START KEYWORD!!!!!!!!!!!!!!!!!!
-        start_keyword = '<h3 id="working-holiday-visa">Working holiday visa</h3>'
-        end_keyword = '<h3'
-        start_pos = html_content.find(start_keyword)
-        if start_pos != -1:
-            end_pos = html_content.find(end_keyword, start_pos + len(start_keyword))
-            if end_pos == -1:
-                end_pos = len(html_content)
-            section_content = html_content[start_pos:end_pos].strip()
-            clean_content = re.sub('<[^>]+>', '', section_content)
-        else: 
-            clean_content = "Section not found"
+     #Search HTML file for ID tags that match parsed entity. 
+        tags_to_search = ['h2', 'h3'] 
+        clean_content = "Section not found"
+
+        for tag in tags_to_search:
+            start_keyword = f'<{tag} id="{entity_names[0]}">'
+            end_keyword = f'<{tag}'
+            start_pos = html_content.find(start_keyword)
+    
+            if start_pos != -1:
+                end_pos = html_content.find(end_keyword, start_pos + len(start_keyword))
+                if end_pos == -1:
+                    end_pos = len(html_content)
+                section_content = html_content[start_pos:end_pos].strip()
+                clean_content = re.sub('<[^>]+>', '', section_content)
+                break  # Break the loop once a match is found
 
         # Return the clean content as the response to Dialogflow
         return jsonify({"fulfillmentText": clean_content})
