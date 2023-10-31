fce6626c712208f41c8991ffbfb76cfaae831acb
Pumamick1 <m.short1@rgu.ac.uk>
2023-10-31 21:02:11 +0000

    Added ability to ask follow-up questions.

--- webhook.py
+++ webhook.py
@@ -3,54 +3,66 @@ import requests
 import re
 
 app = Flask(__name__)
-latest_message = ""
+# Add a global variable to store the latest recognized entity name
+latest_entity_name = ""
 
 def dynamic_text_generator(user_question, scraped_text):
-    # Format the input to DeepAI
-    input_to_deepai = f"Rephrase this text: {scraped_text} so that it answers this question: {scraped_text}. Your answer must include all the information from the text"
-
+    input_to_deepai = f"Answer this question: '{user_question}' using this information: '{scraped_text}'."
     response = requests.post(
         "https://api.deepai.org/api/text-generator",
         data={'text': input_to_deepai},
         headers={'api-key': '7feaf274-c450-40a1-8f90-552928bd1cd0'}
     )
-
     if response.status_code != 200:
-        print("Error:", response.status_code, response.text)
         return "Sorry, I couldn't fetch the required information."
+    return f"Based on our research, {response.json().get('output')}"
 
-    # Extracting the output from DeepAI's response
-    return response.json().get('output')
-
+def extract_active_contexts(payload):
+    return payload.get("queryResult", {}).get("outputContexts", [])
 
 def extract_entities_from_payload(payload):
-    # Extract the parameters (entities) from the payload
+    global latest_entity_name  # Use the global variable
+
     parameters = payload.get("queryResult", {}).get("parameters", {})
+    geo_country = parameters.get("geo-country", "")
     
-    # Find the entities that have values and store their names as strings, excluding 'geo-country'
+    if not geo_country:
+        active_contexts = extract_active_contexts(payload)
+        for context in active_contexts:
+            if 'geo-country' in context['parameters']:
+                geo_country = context['parameters']['geo-country']
+                if geo_country:
+                    break
+
     entity_names = [name for name, value in parameters.items() if value and name != 'geo-country']
 
-    return entity_names
+    # If no entity name is recognized in the current request, use the latest recognized entity name
+    if not entity_names and latest_entity_name:
+        entity_names = [latest_entity_name]
+    elif entity_names:
+        latest_entity_name = entity_names[0]  # Update the global variable if an entity is recognized
+
+    return entity_names, geo_country
 
 @app.route('/webhook', methods=['POST', 'GET'])
 def webhook():
     if request.method == 'GET':
-        return "Webhook is up and running!"  # simple response for GET
-
-    # Default value for user_response
-    user_response = "Sorry, I couldn't fetch the required information."
+        return "Webhook is up and running!"
 
     data = request.get_json()
     api_base_url = 'https://www.gov.uk/api/content/foreign-travel-advice/{}/entry-requirements'
-    geo_country = data['queryResult']['parameters']['geo-country']
+
+    entity_names, geo_country = extract_entities_from_payload(data)
     user_question = data["queryResult"]["queryText"]
     api_specific_url = api_base_url.format(geo_country.lower())
+    print(entity_names)
 
-    entity_names = extract_entities_from_payload(data)
+    if not geo_country:
+        return jsonify({"fulfillmentText": "Sorry, I couldn't identify the country."})
 
     try:
         response_API = requests.get(api_specific_url)
-        response_API.raise_for_status()  # raises an exception for HTTP error codes
+        response_API.raise_for_status()
         data = response_API.json()
 
         html_content = ''
@@ -59,49 +71,30 @@ def webhook():
                 html_content = item.get('body', '')
                 break
 
-        tags_to_search = ['h2', 'h3']
-        clean_content = "Section not found"
+        if not entity_names:  # if no entity was detected in follow-up
+            clean_content = re.sub('<[^>]+>', '', html_content)  # clean the entire content
+            user_response = dynamic_text_generator(user_question, clean_content)
+        else:
+            tags_to_search = ['h2', 'h3']
+            clean_content = "Section not found"
+            for tag in tags_to_search:
+                start_keyword = f'<{tag} id="{entity_names[0]}">'
+                end_keyword = f'<{tag}'
+                start_pos = html_content.find(start_keyword)
 
-        for tag in tags_to_search:
-            start_keyword = f'<{tag} id="{entity_names[0]}">'
-            end_keyword = f'<{tag}'
-            start_pos = html_content.find(start_keyword)
-
-            if start_pos != -1:
-                end_pos = html_content.find(end_keyword, start_pos + len(start_keyword))
-                if end_pos == -1:
-                    end_pos = len(html_content)
-                section_content = html_content[start_pos:end_pos].strip()
-                clean_content = re.sub('<[^>]+>', '', section_content)
-                user_response = dynamic_text_generator(user_question, clean_content)
-                break
+                if start_pos != -1:
+                    end_pos = html_content.find(end_keyword, start_pos + len(start_keyword))
+                    if end_pos == -1:
+                        end_pos = len(html_content)
+                    section_content = html_content[start_pos:end_pos].strip()
+                    clean_content = re.sub('<[^>]+>', '', section_content)
+                    user_response = dynamic_text_generator(user_question, clean_content)
+                    break
 
     except requests.RequestException:
-        # Handle any request errors here (e.g., network issues, invalid URL, etc.)
-        print("Error fetching data from API")
+        user_response = "Sorry, I couldn't fetch the required information."
 
     return jsonify({"fulfillmentText": user_response})
 
-
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
